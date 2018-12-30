89fd2e87e7f68f6578e6367a92440f7a447e7f8b
Steven Leong <steveleong8@gmail.com>
2018-12-29 22:59:25 -0700

    getBadgeClasses

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,15 +3,24 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0
+    // imageURL: "https://picsum.photos/200"
   };
 
   render() {
     return (
+      // react.fragment gets rid of the extra div in inspector
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageURL} alt="" /> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
