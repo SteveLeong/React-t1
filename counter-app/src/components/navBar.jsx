6b21a77417ca91d59be5c459e58b2e7767efd2c8
Steven Leong <steveleong8@gmail.com>
2018-12-30 22:16:04 -0700

    Object Destructuring

--- counter-app/src/components/navBar.jsx
+++ counter-app/src/components/navBar.jsx
@@ -3,13 +3,15 @@ import React, { Component } from "react";
 // Stateless Functional Component
 // this.props only works in class components
 
-const NavBar = props => {
+// objects destructuring
+
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
