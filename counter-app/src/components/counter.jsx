5e755672c9b5f75d6d35a79898b4a0d7643a7fad
Steven Leong <steveleong8@gmail.com>
2018-12-30 20:39:31 -0700

    Binding Event Handlers

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,9 +3,15 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    // imageURL: "https://picsum.photos/200"
+    imageURL: "https://picsum.photos/200",
     tags: []
   };
+
+  // old approach to binding event handlers
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
 
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
@@ -19,29 +25,39 @@ class Counter extends Component {
     );
   }
 
+  // arrow functions inherit 'this'
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
+
   render() {
     return (
       // react.fragment gets rid of the extra div in inspector
       <React.Fragment>
         {/* <img src={this.state.imageURL} alt="" /> */}
-        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button> */}
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()} */}
       </React.Fragment>
     );
   }
 
-  //   getBadgeClasses() {
-  //     let classes = "badge m-2 badge-";
-  //     classes += this.state.count === 0 ? "warning" : "primary";
-  //     return classes;
-  //   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-  //   formatCount() {
-  //     const { count } = this.state;
-  //     return count === 0 ? "Zero" : count;
-  //   }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
